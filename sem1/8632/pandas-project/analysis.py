c62023eb4bc5624da9b60417bdae27d9d607b518
ljjtmm <86856014+ljjtmm@users.noreply.github.com>
2024-05-12 20:23:09 +0100

    function to generate histograms

--- sem1/8632/pandas-project/analysis.py
+++ sem1/8632/pandas-project/analysis.py
@@ -1,6 +1,7 @@
 #Import packages
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 def summarise_variables(data, output_file_name):
     """
@@ -13,9 +14,38 @@ def summarise_variables(data, output_file_name):
             f.write(round(data.describe(), 2).to_string())
 
         print(f"Summary file {output_file_name} successfully generated in script directory.")
-        
+
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+
+def generate_histograms(dataset):
+    """
+    Generate histograms of each variable and save them as PNG files in a directory within the script's directory.
+    """
+
+    try:
+        # Get the directory of the script
+        script_dir = os.path.dirname(os.path.abspath(__file__)) 
+
+        # Create a subdirectory named "images" 
+        images_dir = os.path.join(script_dir, "images") 
+
+        if not os.path.exists(images_dir):
+            os.makedirs(images_dir)
+
+        # Generate the histograms, and exclude the target variable
+        for column in dataset.columns[:-1]:  
+            plt.figure()
+            dataset[column].plot(kind='hist', bins=20, title=f'Histogram of {column}')
+            plt.xlabel(column)
+            plt.ylabel('Frequency')
+            plt.savefig(os.path.join(images_dir, f'{column}_histogram.png'))
+            plt.close()
+        
+        print("Histograms have been successfully generated, and writen to images directory.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
 
 
 
@@ -32,5 +62,8 @@ def main():
     #Write the summary of the variables in the dataset to a file name of our choosing.
     summarise_variables(data, 'variables_summary.txt')
 
+    #Generate histograms
+    generate_histograms(data)
+    
 if __name__ == '__main__':
     main()
